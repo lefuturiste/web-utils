1377fa669ad77bfd9a1a21edbeb55d8bbf669402
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-10-03 14:04:20 +0200

    feat(Logator): more help

--- modules/logator/logator_help.php
+++ modules/logator/logator_help.php
@@ -7,8 +7,20 @@ return [
       'path' => '/loggers'
     ],
     [
+      'name' => 'Create a logger',
+      'path' => '/create'
+    ],
+    [
       'name' => 'Get details about a logger',
       'path' => '/logger-details?id=YOURID'
+    ],
+    [
+      'name' => 'Delete request history for a logger',
+      'path' => '/logger-details?id=YOURID&reset=yes'
+    ],
+    [
+      'name' => 'Delete a logger',
+      'path' => '/logger-details?id=YOURID&delete=yes'
     ],
     [
       'name' => 'Execute a logger',
